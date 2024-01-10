56f352f598731204eff033dbdca9ba2f51b7fd00
alscjf1329 <alscjf1329@gmail.com>
2024-01-10 16:24:42 +0900

    feat) BungeeCord 수신 채널 등록

--- src/main/java/org/dev/securityapiserverbukkit/SecurityAPIServer_Bukkit.java
+++ src/main/java/org/dev/securityapiserverbukkit/SecurityAPIServer_Bukkit.java
@@ -1,17 +1,22 @@
 package org.dev.securityapiserverbukkit;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.dev.securityapiserverbukkit.listener.Channel;
+import org.dev.securityapiserverbukkit.listener.PluginMessageReceiver;
 
 public final class SecurityAPIServer_Bukkit extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getServer().getMessenger()
+            .registerIncomingPluginChannel(this, Channel.BUNGEECORD_CHANNEL,
+                new PluginMessageReceiver());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        this.getServer().getMessenger().unregisterIncomingPluginChannel(this);
     }
 }
